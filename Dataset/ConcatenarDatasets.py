cdfa1bf8a7a45ae8aaa45e68bb6924cc893d52d8
Alpha004 <jesusalren93@gmail.copm>
2024-11-25 23:48:15 -0500

    fix: Food analyzer fix dates, ConcatenarDatasets continues in development

--- Dataset/ConcatenarDatasets.py
+++ Dataset/ConcatenarDatasets.py
@@ -7,7 +7,9 @@ for i in range(1, 16):
 
 # Combinar los DataFrames
 combined_df = pd.concat(datasets, ignore_index=True)
-
+# Retiramos las columnas de glucosa -- COMENTAR ESTA LINEA EN CASO SE QUIERA VALIDAR EL VALOR REAL DE GLUCOSA
+combined_df = combined_df.drop(columns=['glucose','glucose_24h_mean'])
 # Guardar en un archivo CSV
-combined_df.to_csv('main_dataset.csv', index=False)
+combined_df.to_csv('main_dataset_without_glucose.csv', index=False)
+# combined_df.to_csv('main_dataset.csv', index=False)
 print('Combinacion finalizada!')
